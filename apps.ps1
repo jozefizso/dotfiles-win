6b1bbe7d49b4d028f83d30c6f964f55d6c86b485
Jozef Izso <jozef.izso@gmail.com>
2019-01-11 14:07:46 +0100

    List of default applications to install on dev machine

---
+++ apps.ps1
@@ -0,0 +1,17 @@
+#
+# Apps for development machine
+#
+
+choco install -y firefox
+
+choco install -y git
+choco install -y 7zip
+choco install -y notepad2-mod
+choco install -y totalcommander
+
+choco install -y sublimetext3
+# choco install -y sublimemerge
+
+choco install -y jdk8 --x86
+choco install -y maven
+choco install -y ant
