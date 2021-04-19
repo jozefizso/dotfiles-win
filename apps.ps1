99f83e2cee2ae0960045c8a13a0db56a02a891fa
Jozef Izso <jozef.izso@gmail.com>
2021-04-19 16:36:39 +0200

    Enable Sublime Merge

--- apps.ps1
+++ apps.ps1
@@ -8,13 +8,10 @@ choco install -y git
 choco install -y 7zip
 choco install -y notepad2-mod
 choco install -y totalcommander
+choco install -y sublimemerge
+choco install -y winmerge
 
 choco install -y cascadiamono
-
-choco install -y sublimetext3
-# choco install -y sublimemerge
-
-choco install -y winmerge
 
 choco install -y minishift
 choco install -y openshift-cli
